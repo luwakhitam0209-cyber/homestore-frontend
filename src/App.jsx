3d36986d8f442c69de7bebbdc041ea200694ca7b
resta <restaa@gmail.com>
2026-08-31 14:40:07 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
 
       <main>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
 }
 
