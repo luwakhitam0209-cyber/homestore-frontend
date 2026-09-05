fba4866a371a6f480c4cef8b2516a614ab4aae35
resta <restaa@gmail.com>
2026-09-05 11:26:29 +0700

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.162.84.185:8000/api",
+  baseURL: "http://localhost:8000/api",
   headers: {
     Accept: "application/json",
   },
