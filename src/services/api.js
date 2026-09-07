eadc972308086a7a62ff6974b30e215e20087068
unknown <diana2809@gmail.com>
2026-09-07 13:07:30 +0700

    updateeee api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://count-registered-earn-ryan.trycloudflare.com/api",
+  baseURL: "https://attract-officers-count-rats.trycloudflare.com/api",
   headers: {
     Accept: "application/json",
   },
